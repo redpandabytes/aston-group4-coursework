c52e61e7296f537d8f7e213da0064a43ec80ca56
Dehul <dazzle54825@gmail.com>
2016-11-16 23:44:42 +0000

    did stuff

---
+++ Assets/Scripts/New Scripts/menu_Main/Scroll.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class Scroll : MonoBehaviour {
+
+    public RectTransform panel; // Holds the panel
+    public RectTransform center; // center position
+    public Image[] buttons; // Array of images ie the buttons
+    public float[] distance; // Distance of all buttons from the center 
+
+    private bool dragging = false; //Only true if image is being dragged
+    private int closestButton; // closest button to the center
+    
+	// Use this for initialization
+	void Start ()
+    {
+        int bttnLength = buttons.Length;
+        distance = new float[bttnLength];
+
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+        for (int i = 0; i < buttons.Length; i++)
+        {
+
+            distance[i] = Mathf.Abs(center.transform.position.x - buttons[i].transform.position.x);
+
+            //Console.WriteLine(distance[i]);
+        
+        }
+
+        
+	}
+
+    public void startDrag()
+    {
+        dragging = true;
+    }
+
+    public void endDragging()
+    {
+        dragging = false;
+    }
+
+}
--- Assets/Scripts/New Scripts/menu_Main/ScrollRectSnap.cs
+++ Assets/Scripts/New Scripts/menu_Main/ScrollRectSnap.cs
@@ -1,37 +1,66 @@
 ﻿// @Author: Dehul Shingadia
 
+
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+
+
 
 public class ScrollRectSnap : MonoBehaviour {
     //Public Variables
     public RectTransform Main_Panel; // Will hold the scrollPanel
     public Image[] bttn;
     public RectTransform center;// Center to compate the distance form each button
+    public float[] distance;// All buttons distance from the center
+    public float[] distReposition;
 
     //Private Variables
-    private float[] distance;// All buttons distance from the center
+    
     private bool dragging = false;// will only be true if panel is being dragged
-    private int bttnDistance;// The distance between the buttons
+    private float bttnDistance;// The distance between the buttons
     private int minButtonNum;// holds the button number which is closest to the center
+    private int bttnLength;
+
 
     void start()
     {
 		int bttnLength = bttn.Length;
         distance = new float[bttnLength];
+        distReposition = new float [ bttnLength ];
+
+        
+        bttnDistance = bttn[1].transform.position.x - bttn[0].transform.position.x;
+        
 
         //getting the distance between buttons
-       bttnDistance = (int)Mathf.Abs(bttn[1].GetComponent<RectTransform>().anchoredPosition.x - bttn[0].GetComponent<RectTransform>().anchoredPosition.x);
-       
+        bttnDistance = (int)Mathf.Abs(bttn[1].GetComponent<RectTransform>().anchoredPosition.x - bttn[0].GetComponent<RectTransform>().anchoredPosition.x);
+
     }
 
     void update()
-    {
+    { 
+           
+
         for (int i = 0; i < bttn.Length; i++)
         {
-            distance[i] = Mathf.Abs(center.transform.position.x - bttn[i].transform.position.x);
+            distReposition[i] = center.GetComponent<RectTransform>().position.x - bttn[i].GetComponent<RectTransform>().position.x;
+            distance[i] = Mathf.Abs(distReposition[i]); ///center.transform.position.x - bttn[i].transform.position.x
+
+            if (distReposition[i] > 1000)
+            {
+                float curX = bttn[i].GetComponent<RectTransform>().anchoredPosition.x;// current x positioon of button
+                float cury = bttn[i].GetComponent<RectTransform>().anchoredPosition.y;// current x positioon of button
+                System.Console.WriteLine("button: " + bttn[i]  + curX);
+                System.Console.WriteLine("button: " + bttn[i]  + cury);
+                Vector2 newAnchoredPos = new Vector2(curX + (bttnLength * bttnDistance), cury);
+                bttn[i].GetComponent<RectTransform>().anchoredPosition = newAnchoredPos;
+             
+            }
+            
         }
+
+
 
         float minDistance = Mathf.Min(distance);//gets min distance
 
@@ -45,7 +74,7 @@ public class ScrollRectSnap : MonoBehaviour {
 
             if (!dragging)
             {
-                LerpToBttn(minButtonNum * -bttnDistance);
+                //LerpToBttn(minButtonNum * -bttnDistance);
             }
         }
     }
