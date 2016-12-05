48b79fce0b2ffc0049ff951a1a6f6a6a9e270afe
mattykav <mattykav22@gmail.com>
2016-12-05 17:56:22 +0000

    pause menu

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuScript : MonoBehaviour {
+	public Transform canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (canvas.gameObject.activeInHierarchy == false) {
+				canvas.gameObject.SetActive (true);
+				Time.timeScale = 0;
+			} else {
+				canvas.gameObject.SetActive (false);
+				Time.timeScale = 1;
+			}
+		}
+
+	}
+
+	//public void ResumePlayBtn() {
+	//	canvas.gameObject.activeInHierarchy == false;
+	//}
+}
