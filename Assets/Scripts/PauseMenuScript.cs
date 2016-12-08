ec77289c1bb6a79c775278ec1781eeb82fd96708
mattykav <mattykav22@gmail.com>
2016-12-08 15:48:43 +0000

    buggy pause

--- Assets/Scripts/PauseMenuScript.cs
+++ Assets/Scripts/PauseMenuScript.cs
@@ -3,28 +3,31 @@ using System.Collections;
 
 public class PauseMenuScript : MonoBehaviour {
 	public Transform canvas;
-
+	public Transform menu;
+	public bool b = false;
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//if (Input.GetKeyDown (KeyCode.P)) {
-		if (Input.GetMouseButtonDown(0)) {
-			if (canvas.gameObject.activeInHierarchy == false) {
+		//if (Input.GetMouseButtonDown (0)) {
+			if (Input.GetKeyDown (KeyCode.P)) {
+			if (canvas.gameObject.activeInHierarchy == false && menu.gameObject.activeInHierarchy == false && b == true) {
 				canvas.gameObject.SetActive (true);
+				menu.gameObject.SetActive (true);
 				Time.timeScale = 0;
 			} else {
 				canvas.gameObject.SetActive (false);
+				menu.gameObject.SetActive (false);
 				Time.timeScale = 1;
 			}
 		}
-
 	}
-
-	//public void ResumePlayBtn() {
-	//	canvas.gameObject.activeInHierarchy == false;
-	//}
+	public void clickedBtn() {
+		b = true;
+		Update ();
+		}
 }
+
