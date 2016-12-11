3bc9b02236544d9381aaff2d0d3c0cb801b58fc4
Dehul <dazzle54825@gmail.com>
2016-12-11 15:56:16 +0000

    cards can be picked up and played!!!

--- Assets/Scripts/_Reusable Components/Draggable.cs
+++ Assets/Scripts/_Reusable Components/Draggable.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿/*
+ * "Author: Dehul Shingadia 
+ */
+
+using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
 using System;
@@ -8,17 +12,21 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     Vector3 startPosition;
     Transform startParent;
 
+    public Transform parentToReturnTo = null;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
 
-        Debug.Log("Hello");
+        Debug.Log("Card Picked Up");
         //itemBeingDragged = gameObject;
+
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("Hello 2");
+        Debug.Log("Card Being Dragged");
         //throw new NotImplementedException();
 
         this.transform.position = eventData.position;//position of mouse
@@ -26,7 +34,8 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("Hello 3");
+        Debug.Log("Card Dropped");
+        this.transform.SetParent(parentToReturnTo);
         //throw new NotImplementedException();
     }
 
---
+++ Assets/Scripts/_Reusable Components/DropZone.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * @Author: Dehul Shingadia
+ */
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " Dropped on: " + gameObject.name);
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null) {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+}
