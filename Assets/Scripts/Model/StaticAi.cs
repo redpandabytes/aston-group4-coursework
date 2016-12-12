d5ad7a73f168841b3fe49d558c2c55edd727646d
rahimkhano <rahimakhanom@live.co.uk>
2016-12-12 17:15:31 +0000

    fixing

--- Assets/Scripts/Model/StaticAi.cs
+++ Assets/Scripts/Model/StaticAi.cs
@@ -3,57 +3,70 @@
     //Fields
     private int _difficulty;
     private int _noOfPlayers;
+    private Card cardInPlay;
+    private int[] playersHandSizes;
 
     public void Initialise(int difficulty, int noOfPlayers)
     {
         this._difficulty = difficulty;
         this._noOfPlayers = noOfPlayers;
+        for (int i = 0; i < noOfPlayers; i++)
+        {
+            playersHandSizes[i] = 7;
+        }
+        cardInPlay = null;
     }
-    /* private int difficulty; //1 = easy, 2 = medium, 3 = hard
-private Card cardInPlay;
-private int[] playersHandSizes;
 
 //Initialization of AIController
-private void Start(int difficulty, int noOfPlayers)
+private void Start()
 {
-    this.difficulty = difficulty;
-   // playersHandSizes = int[noOfPlayers];
-    cardInPlay = null;
-    for (int i = 0; i < noOfPlayers; i++)
-    {
-        playersHandSizes[i] = 7;
-    }
 }
 
 //Method for when player turn is passed to AI
-public Action AITurn(Player x, int playerNo)
+/*public Action AITurn(Player x, int playerNo)
 {
-    if (difficulty == 3)
+    if (_difficulty == 3)
     {
         hardPlay();
     }
-    else if (difficulty == 2)
+    else if (_difficulty == 2)
     {
         mediumPlay();
     }
-    else
-    {
-        easyPlay();
+    else {
+        easyPlay(x, playerNo);
     }
-}
+} */
+
 //Easy AI turn
 public Action easyPlay(Player x, int playerNo)
 {
     Player p = x;
     int pNo = playerNo;
-    //GET HAND METHOD NEEDED
-    x.getHand() == Hand h;
-     if(hand.size == 1){
-             if(hand[0].getValye() <// 10 && ((hand[0].getValue() || hand[0].getGuild() ==currentcrd.getguild());
+    Hand h = p.getHand();
+        if (h.getHandSize() == 1) {
+            Card c = h.getCardAtIndex(0);
+            if ((c.getValue() < 10) && ((c.getValue() == cardInPlay.getValue()) || c.getGuild() == cardInPlay.getGuild())){
+                Action a = new Action();
+                a.Initialise("playCard", c);
+                return a;
+            } else {
+                Action a = new Action();
+                a.Initialise("pickUp", null);
+                return a;
+            }
+        } else {
+            Action a = new Action();
+            a.Initialise("pickUp", null);
+            return a;
+        }
+}
+    public Action mediumPlay() {
+
+        return new Action();
+    }
+    public Action hardPlay() {
+        return new Action();
      }
-}
-public Action mediumPlay() { }
-public Action hardPlay() { }
-public Action updateAIKnowledge() { }
-} */
-}
+    public void updateAIKnowledge() { }
+} 
