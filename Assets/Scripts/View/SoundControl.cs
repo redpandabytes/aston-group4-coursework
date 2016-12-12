d085ef0c078ed7ab3f919a41178f1ddb6a1c8f56
mattykav <mattykav22@gmail.com>
2016-12-12 15:53:45 +0000

    me and david made mute

---
+++ Assets/Scripts/View/SoundControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundControl : MonoBehaviour {
+	AudioSource audio;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+	}
+
+	public void mute()
+	{
+		audio.mute = true;
+	}
+
+	public void unMute()
+	{
+		audio.mute = false;
+	}
+}
