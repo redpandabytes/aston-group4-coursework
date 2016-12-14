d21f4b211628537c19f4c8b6f9262ab076f2fd94
Nathaniel Baulch-Jones <baulchjn@aston.ac.uk>
2016-12-14 07:26:37 +0000

    Fixed Error Preventing Android Build

--- Assets/Scripts/View/MainMenubuttonManager.cs
+++ Assets/Scripts/View/MainMenubuttonManager.cs
@@ -35,8 +35,7 @@ public class MainMenubuttonManager : GuildsElement
     }
     public void btnExit()
     {
-       // Application.Quit();
-		UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
     }
 
     public void toHelpBtn(string toHelp)
