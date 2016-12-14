dc47fe69d27e23ecd5e378e38334426827784479
anntomi <anntomi@hotmail.co.uk>
2016-12-14 09:33:25 +0000

    MusicBetweenScenes

---
+++ Assets/Scripts/View/DontDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+//@author: Ann Tomi
+public class DontDestroy : MonoBehaviour {
+
+	void Awake()
+	{
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("music");
+		if (objs.Length > 1)
+			Destroy (this.gameObject);
+		DontDestroyOnLoad (this.gameObject);
+
+
+	}
+		
+}
