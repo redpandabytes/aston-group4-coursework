7a90a3573ecad386cb6ac0a0b271959608be8d94
Dehul Shingadia <dazzle54825@gmail.com>
2017-02-19 15:18:57 +0000

    victory and defeat scenes animated, code cleaned up, new font for both scenes

--- Assets/Scripts/View/AnimationsTimeScale.cs
+++ Assets/Scripts/View/AnimationsTimeScale.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
+﻿/* 
+* @Author: Dehul Shingadia
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationTrigger : MonoBehaviour {
+public class AnimationsTimeScale : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
         Time.timeScale = 1;
         //needed for animatiosn to work
-        
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
