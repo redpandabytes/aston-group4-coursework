5fe59f0ae510a5f416a0e897755de14e7b1cc801
mattykav <mattykav22@gmail.com>
2017-03-13 13:46:47 +0000

    slider works

--- Assets/Scripts/View/SoundControl.cs
+++ Assets/Scripts/View/SoundControl.cs
@@ -4,34 +4,45 @@
  */
 
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SoundControl : MonoBehaviour {
-	AudioSource audio;
+	GameObject[] audioArray;
+    GameObject audioSource;
+     
     float volume;
 	// Use this for initialization
 	void Start () {
-		audio = GetComponent<AudioSource> ();
-       // volume = GameObject.Find("sound").GetComponent<AudioSource>().volume;
+		audioArray = GameObject.FindGameObjectsWithTag("music");
+        audioSource = audioArray[0];
+        // volume = GameObject.Find("sound").GetComponent<AudioSource>().volume;
         //PlayerPrefs.SetFloat("Volume", volume);
         //AudioListener.volume = PlayerPrefs.GetFloat("volume");
     }
 
 	public void mute()
 	{
-		audio.mute = true;
+		//audio.mute = true;
 	}
 
 	public void unMute()
 	{
-		audio.mute = false;
+		//audio.mute = false;
 	}
 
     public void Update()
     {
-        AudioListener.volume = 0.1f;
+        Debug.Log(audioSource.GetComponent<AudioSource>().volume);
     }
-   
+    public void OnValueChanged(float newValue)
+    {
+        //audioSource.GetComponent<AudioSource>().volume = 0.1f;
+        Debug.Log(newValue);
+        var slider = GameObject.Find("Volume_Slider");
+        var volumeVal = slider.GetComponent<Slider>().value;
+        GameObject.FindGameObjectsWithTag("music")[0].GetComponent<AudioSource>().volume = volumeVal;
+    }
 }
 
 
