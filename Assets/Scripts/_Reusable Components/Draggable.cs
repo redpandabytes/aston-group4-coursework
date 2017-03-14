0c17ee429b477e4fc5f651e6cc56bf2850c94933
Dehul <dazzle54825@gmail.com>
2017-03-14 14:30:02 +0000

    need to refactor zoom code, semi-working

--- Assets/Scripts/_Reusable Components/Draggable.cs
+++ Assets/Scripts/_Reusable Components/Draggable.cs
@@ -11,7 +11,10 @@ public class Draggable : GuildsElement, IBeginDragHandler, IDragHandler, IEndDra
 {
     public Transform parentToReturnTo = null;
     public Boolean draggable = true;
-    
+    public float tapCooldown = 0.001f;
+    private int tapCount = 0;
+    public Boolean large = false;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         if (draggable == true)
@@ -19,10 +22,10 @@ public class Draggable : GuildsElement, IBeginDragHandler, IDragHandler, IEndDra
             if (this.transform.parent == GameObject.Find("playedCardsCell").transform)
             {
                 draggable = false;
-            } else { 
-              Debug.Log("Card Picked Up");
-              parentToReturnTo = this.transform.parent;
-              this.transform.SetParent(this.transform.parent.parent);
+            } else {
+                Debug.Log("Card Picked Up");
+                parentToReturnTo = this.transform.parent;
+                this.transform.SetParent(this.transform.parent.parent);
             }
         }
     }
@@ -38,22 +41,66 @@ public class Draggable : GuildsElement, IBeginDragHandler, IDragHandler, IEndDra
     public void OnEndDrag(PointerEventData eventData)
     {
         int player = app.model.GetCurrentPlayer();
-       
-            if (draggable == true)
-            {
-                this.transform.SetParent(parentToReturnTo);
-                //if card is in center make uninteractable
 
-                if (this.transform.position == GameObject.Find("Hand0").transform.position)
-                {
-                    draggable = false;
-                }
-                else
-                {
-                    draggable = true;
-                }
+        if (draggable == true)
+        {
+            this.transform.SetParent(parentToReturnTo);
+            //if card is in center make uninteractable
+
+            if (this.transform.position == GameObject.Find("Hand0").transform.position)
+            {
+                draggable = false;
             }
-      }
+            else
+            {
+                draggable = true;
+            }
+        }
+    }
 
   
+    void Update()
+    {
+        var GameAction = new GameAction();
+        bool zoom = false;
+        
+        if (Input.anyKeyDown)
+        {
+            if (tapCooldown > 0 && tapCount == 2)
+            {
+                if (zoom == false)
+                {
+                    //double tapped
+                    Debug.Log("Double TAPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP");
+                    // make card much larger
+                    // yield return new WaitForSeconds(1.0f);
+                    //zoom = true;
+                }
+            }
+            else if (tapCooldown >0 && tapCount == 2)
+            {
+                if (zoom == true)
+                {
+                Debug.Log("unzoom");
+                //unzoom
+                //zoom = false;
+                }
+            }
+            else
+            {
+                tapCount += 1;
+            }
+        }
+
+        if (tapCooldown > 0)
+        {
+            tapCooldown -= 1 * Time.deltaTime;
+
+        }
+        else {
+            tapCount = 0;
+        }
+    }
+    // refactor update method into multiple functions to prevent onclick being detected in multiple frames emulating multiple clicks
 }
+  
